2ab0deaf4c8433c947238f3c38a15021e805db4b
Akbota <ak_askarova@kbtu.kz>
2026-05-18 00:32:01 +0500

    Refactor System package

---
+++ Desktop/oop-exam-project-main/oop-exam-project-main/src/main/java/System/Report.java
@@ -0,0 +1,39 @@
+package edu.university.system;
+ 
+import java.io.Serializable;
+import java.util.Date;
+ 
+public class Report implements Serializable {
+    private static final long serialVersionUID = 1L;
+ 
+    private String title;
+    private String content;
+    private Date createdDate;
+ 
+    public Report(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.createdDate = new Date();
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    public String getContent() {
+        return content;
+    }
+ 
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+ 
+    public String generate() {
+        return title + "\nCreated: " + createdDate + "\n" + content;
+    }
+ 
+    @Override
+    public String toString() {
+        return generate();
+    }
+}
